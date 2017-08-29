e9b4c01a758a05627c6fb52e8f9f681578cbe7a0
chavp <my.parinya@gmail.com>
2017-08-29 16:17:28 +0700

    add gathering data
    Signed-off-by: chavp <my.parinya@gmail.com>

--- Autotrade.Recomender.Tests/Autotrade.Recomender/Program.cs
+++ Autotrade.Recomender.Tests/Autotrade.Recomender/Program.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using CsvHelper;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -13,10 +15,12 @@ namespace Autotrade.Recomender
     {
         static void Main(string[] args)
         {
-            decimal previes_price = 0;
+            decimal? previese_price = null;
             decimal current_price = 0;
             decimal next_price = 0;
             string direction = "";
+            string data_file = DateTime.Today.ToString("ddMMyyyy");
+
             while (true)
             {
                 var client = new HttpClient();
@@ -30,24 +34,43 @@ namespace Autotrade.Recomender
                 response.Wait();
                 if (response.IsCompleted)
                 {
+                    data_file = Path.Combine(DateTime.Today.ToString("ddMMyyyy")) + ".csv";
+                    if (!File.Exists(data_file))
+                    {
+                        File.AppendAllText(data_file, "TIME, DIRECTION, CURRENT_PRICE" + Environment.NewLine);
+                        previese_price = -1;
+                    }
+                    else if(previese_price == null)
+                    {
+                        using (var reader = new CsvReader(File.OpenText(data_file)))
+                        {
+                            reader.Configuration.IgnoreHeaderWhiteSpace = true;
+                            var lastRecord = reader.GetRecords<PricingData>().LastOrDefault();
+                            previese_price = lastRecord.CURRENT_PRICE;
+                        }
+                    }
                     var re = response.Result.Content.ReadAsAsync<CapfeedResult>().Result;
                     //var data = JsonConvert.DeserializeObject<dynamic>(re);
-                    direction = ".";
                     foreach (var item in re.result)
                     {
                         current_price = item.usdPrice;
-                        if(current_price > previes_price)
+                        var diffPrice = current_price - previese_price;
+                        if (diffPrice > 0)
                         {
                             direction = "+";
+                            if (previese_price == -1) direction = "*";
                             Console.Write(direction);
+                            File.AppendAllText(data_file, getdata(direction, current_price));
                         }
-                        else if (current_price < previes_price)
+                        else if (diffPrice < 0)
                         {
                             direction = "-";
                             Console.Write(direction);
+                            File.AppendAllText(data_file, getdata(direction, current_price));
                         }
+
+                        previese_price = current_price;
                         
-                        previes_price = current_price;
                     }
                 }
 
@@ -55,6 +78,12 @@ namespace Autotrade.Recomender
             }
         }
 
+        private static string getdata(string direction, decimal current_price)
+        {
+            return string.Format(
+            "{0}, {1}, {2}" + Environment.NewLine, 
+            DateTime.Now.TimeOfDay, direction, current_price);
+        }
         public struct CapfeedResult
         {
             //"mktcap": "32694478411", "supply": "94299480", "change24": "0.7" } ]}
@@ -74,6 +103,13 @@ namespace Autotrade.Recomender
             public decimal mktcap { get; set; }
             public decimal supply { get; set; }
             public decimal change24 { get; set; }
+        }
+
+        public struct PricingData
+        {
+            public TimeSpan TIME { get; set; }
+            public string DIRECTION { get; set; }
+            public decimal CURRENT_PRICE { get; set; }
         }
     }
 }
